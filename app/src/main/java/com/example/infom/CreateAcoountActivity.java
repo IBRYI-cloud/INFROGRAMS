88d4c1f7ca0d9599f35922971ae3f733444b8b0c
IBRYI <bryf302@gmail.com>
2021-09-02 11:53:13 -0400

    intent

---
+++ app/src/main/java/com/example/infom/CreateAcoountActivity.java
@@ -0,0 +1,13 @@
+package com.example.infom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAcoountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_acoount);
+    }
+}
